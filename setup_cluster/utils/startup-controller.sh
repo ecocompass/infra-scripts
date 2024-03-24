758f7a7db2ed5828371f509315594f1b3d012e90
sakshi-dhingra <sakshi2210dhingra@gmail.com>
2024-03-24 11:56:29 +0000

    fixed k8s installation issue

--- setup_cluster/utils/startup-controller.sh
+++ setup_cluster/utils/startup-controller.sh
@@ -4,11 +4,10 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y docker.io
 sudo systemctl enable docker.service
-sudo apt install -y apt-transport-https curl
-curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo apt install -y apt-transport-https curl gpg ca-certificates
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 
 # install k8s core
